475d98dfbd05ffb8d24d1cf82db0b691e7d7afbd
Empty <emptypageworking@gmail.com>
2026-08-06 00:05:35 +0300

    Style(item-list): added style for item list

--- client/src/components/shared/listItems/ListItems.tsx
+++ client/src/components/shared/listItems/ListItems.tsx
@@ -3,18 +3,18 @@ import './ListItems.css'
 
 export default function ListItems() {
   const items = [
-    {id: 1, title: '1'},
-    {id: 2, title: '2'},
-    {id: 3, title: '3'},
+    {id: 1, title: 'item'},
+    {id: 2, title: 'item'},
+    {id: 3, title: 'item'},
   ]
 
   return (
     <div>
-      <h2>Items List</h2>
+      <h2 className='title-h2-list-item'>Items List</h2>
       <div>
         {items.map((items) => (
           <div key={items.id}>
-            <div className=''>  
+            <div className='item-panel-text'>  
               <p>{items.id}</p>
               <p>{items.title}</p>
             </div>
