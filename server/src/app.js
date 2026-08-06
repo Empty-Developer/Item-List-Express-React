ceceb75e2f1fec6fdfd5ba0868abb19cccd0370a
Empty <emptypageworking@gmail.com>
2026-08-06 06:22:42 +0300

    Fix(render) test 5

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://item-list-express-react.vercel.app/",
 }));
 const PORT = process.env.PORT || "8080";
 
